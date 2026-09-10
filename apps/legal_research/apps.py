7e2e3c163f255b4f6a8b6fce0604becf0d4f65a8
Admin <admin@medicore.local>
2026-09-10 16:26:57 +0530

    feat(research): implement StatutoryAct and StatutorySection models

---
+++ apps/legal_research/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class LegalResearchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.legal_research'
+    verbose_name = 'Legal Research Workspace & Precedents'
